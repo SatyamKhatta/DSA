54409cedeccb01f3cb47b71f45a76d288d48caa9
SatyamKhatta <149493210+SatyamKhatta@users.noreply.github.com>
2025-02-07 21:52:24 +0530

    find all divisor of number

---
+++ alldivisor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int sumOfDivisors(int n) {
+    int sum = 0;
+
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            sum += i;  // Add the divisor
+            if (i != n / i) {  // Add the paired divisor (avoid duplicate if i == n/i)
+                sum += n / i;
+            }
+        }
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    
+    cout << "Sum of all divisors of " << n << " is: " << sumOfDivisors(n) << endl;
+    return 0;
+}
